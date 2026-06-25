5162cc4243579eb4c2ab3ce351486e1e8db83e66
ctyhach25 <tyhachc@oregonstate.edu>
2026-06-24 22:06:34 -0700

    Initial Commit

---
+++ string_distance.py
@@ -0,0 +1,12 @@
+# Author: Caitlyn Tyhach
+# Git ID: ctyhach25
+# Date: 6/24/2026
+# Description: This module uses the Levenshtein distance between strings to say how similar they are (in text, not in meaning).
+
+from fuzzywuzzy import fuzz
+
+def string_distance(string_list, target_string):
+    results = {}
+    for s in string_list:
+        results[s] = fuzz.ratio(s, target_string)
+    return results
